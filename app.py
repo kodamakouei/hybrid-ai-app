1fcdc969e4445e836f68f3af8cc8e043ba4a9cb5
kodama koei <is251036@cis.aichi-pu.ac.jp>
2025-11-15 13:54:37 +0900

    ファイルで会話

--- app.py
+++ app.py
@@ -87,7 +87,7 @@ header {{ visibility: hidden; }}
     width: {SIDEBAR_FIXED_WIDTH} !important;
     min-width: {SIDEBAR_FIXED_WIDTH} !important;
     max-width: {SIDEBAR_FIXED_WIDTH} !important;
-    background-color: #cccccc;
+    background-color: #f1f1f1;
     overflow-x: hidden !important; 
     overflow-y: hidden !important; 
 }}
